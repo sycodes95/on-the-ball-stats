ba7eb36b22dbdcff285c8f3708b6f6191ff13b80
sycodes95 <sycodes95@gmail.com>
2023-08-15 23:06:26 -0700

    feat : improved fixture timeline visuals and responsiveness

--- src/features/fixtureStats/components/statsView/timeLine/timeLine.tsx
+++ src/features/fixtureStats/components/statsView/timeLine/timeLine.tsx
@@ -1,7 +1,8 @@
+import Icon from "@mdi/react";
 import { Fixture } from "../../../../homepage/types/types";
 import { fixtureTimelineEventsImages } from "../../../constants/constants";
 import { hasFixtureStarted } from "../../../utils/hasFixtureStarted";
-
+import { mdiArrowUpBoldBoxOutline } from '@mdi/js';
 type TimeLineProps = {
   fixture : Fixture;
 }
@@ -19,24 +20,48 @@ function TimeLine ({fixture} : TimeLineProps) {
           ev.team.id === fixture.teams.home.id &&
           
           <div className="flex items-center justify-end w-full gap-2">
-            <span>{ev.detail}</span>
+            {/* If no player is involved in the event, display event detail */}
             {
             ev.detail && ev.type === 'Card' && 
-            <img className="object-contain w-6 h-6" src={fixtureTimelineEventsImages[ev.detail]} alt="" />
+            <div className="flex">
+              <span className="p-1 rounded-md">{ev.player.name ? ev.player.name : ev.detail}</span>
+              <img className="object-contain w-6 h-6" src={fixtureTimelineEventsImages[ev.detail]} alt="" />
+            </div>
             }
             {
             ev.detail && ev.type === 'subst' &&
-            <img className="object-contain w-6 h-6" src={fixtureTimelineEventsImages[ev.type]} alt="" />
+            <div className="flex flex-col gap-2 md:flex-row">
+              
+
+              <div className="flex items-center justify-end gap-1">
+                <span className="p-1 rounded-md">{ev.assist.name}</span>
+                <Icon className="w-6 h-6 text-emerald-600 text-opacity-80" path={mdiArrowUpBoldBoxOutline} />
+              </div>
+
+              <div className="flex items-center justify-end gap-1">
+                <span className="p-1 rounded-md">{ev.player.name ? ev.player.name : ev.detail}</span>
+                <Icon className="w-6 h-6 text-red-600 text-opacity-50 rotate-180" path={mdiArrowUpBoldBoxOutline} />
+              </div>
+            </div>
             }
             {
             ev.detail && ev.type === 'Goal' &&
-            <img className="object-contain w-6 h-6" src={fixtureTimelineEventsImages[ev.type]} alt="" />
+            <div className="flex gap-2">
+              <span className="p-1 rounded-md">{ev.player.name ? ev.player.name : ev.detail}</span>
+              <img className="object-contain w-6 h-6" src={fixtureTimelineEventsImages[ev.type]} alt="" />
+            </div>
+            }
+            {
+            ev.detail && !ev.player.name && ev.detail &&
+            <div className="flex gap-2">
+              <span className="p-1 rounded-md">{ev.detail}</span>
+            </div>
             }
           </div>
           }
 
-          <div className="flex justify-center w-full col-start-2 text-white ">
-            <span className="w-8 p-1 text-center rounded-md bg-slate-400">{ev.time.elapsed}'</span>
+          <div className="flex justify-center w-full col-start-2 text-white h-full6">
+            <span className="w-8 h-6 p-1 text-center rounded-md bg-slate-400">{ev.time.elapsed}'</span>
           </div>
 
           {
@@ -45,18 +70,41 @@ function TimeLine ({fixture} : TimeLineProps) {
           <div className="flex items-center justify-start w-full gap-2">
             {
             ev.detail && ev.type === 'Card' && 
-            <img className="object-contain w-6 h-6" src={fixtureTimelineEventsImages[ev.detail]} alt="" />
+            <div className="flex overflow-hidden whitespace-nowrap text-ellipsis">
+              <img className="object-contain w-6 h-6" src={fixtureTimelineEventsImages[ev.detail]} alt="" />
+              <span className="p-1 rounded-md ">{ev.player.name ? ev.player.name : ev.detail}</span>
+            </div>
             }
             {
             ev.detail && ev.type === 'subst' &&
-            <img className="object-contain w-6 h-6" src={fixtureTimelineEventsImages[ev.type]} alt="" />
+            <div className="flex flex-col gap-2 md:flex-row">
+              <div className="flex items-center justify-start gap-1">
+                <Icon className="w-6 h-6 text-emerald-600 text-opacity-80" path={mdiArrowUpBoldBoxOutline} />
+                <span className="p-1 rounded-md">{ev.assist.name}</span>
+              </div>
+              <div className="flex items-center justify-start gap-1">
+                <Icon className="w-6 h-6 text-red-600 text-opacity-50 rotate-180" path={mdiArrowUpBoldBoxOutline} />
+                <span className="p-1 rounded-md">{ev.player.name ? ev.player.name : ev.detail}</span>
+              </div>
+
+              
+
+            </div>
             }
             {
             ev.detail && ev.type === 'Goal' &&
-            <img className="object-contain w-6 h-6" src={fixtureTimelineEventsImages[ev.type]} alt="" />
+            <div className="flex gap-2">
+              <img className="object-contain w-6 h-6" src={fixtureTimelineEventsImages[ev.type]} alt="" />
+              <span className="p-1 rounded-md">{ev.player.name ? ev.player.name : ev.detail}</span>
+            </div>
+            }
+            {
+            ev.detail && (ev.type !== 'Card' && ev.type !== 'subst' && ev.type !== 'Goal' ) &&
+            <div className="flex gap-2">
+              <span className="p-1 rounded-md">{ev.detail}</span>
+            </div>
             }
             
-            <span>{ev.detail}</span>
           </div>
           }
         
