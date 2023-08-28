f6594517b9e6f476d15b9ccfb7e24f889dc01f57
sycodes95 <sycodes95@gmail.com>
2023-08-27 21:15:29 -0700

    feat: made team icons clickable head to head view in fixture stats

--- src/features/fixtureStats/components/headToHeadView/h2HBasicStats/h2HBasicStats.tsx
+++ src/features/fixtureStats/components/headToHeadView/h2HBasicStats/h2HBasicStats.tsx
@@ -2,6 +2,8 @@ import H2HWinPctCircleBarHome from "./h2HWinPctCIrcleBar/h2HWinPctCircleBarHome"
 import H2HWinPctCircleBarAway from "./h2HWinPctCIrcleBar/h2HWinPctCircleBarAway";
 import H2HDrawPctCircleBar from "./h2HWinPctCIrcleBar/h2HDrawPctCircleBar";
 import { Fixture } from "../../../../../types/types";
+import { Link } from "react-router-dom";
+import { getURLFriendlyString } from "../../../../../utils/getURLFriendlyString";
 
 type H2HBasicStatsProps = {
   headToHeadFixtures: Fixture[];
@@ -12,13 +14,17 @@ function H2HBasicStats ({headToHeadFixtures} : H2HBasicStatsProps) {
   return (
     <div className="flex items-center justify-center w-full">
       <div className="flex items-center gap-4">
-        <img className="object-contain w-12 h-12" src={headToHeadFixtures[0]?.teams.home.logo} alt="" />
+        <Link className="hover:opacity-70" to={`/team/${headToHeadFixtures[0].league.id}/${headToHeadFixtures[0].teams.home.id}/${getURLFriendlyString(headToHeadFixtures[0].teams.home.name)}`}>
+          <img className="object-contain w-12 h-12" src={headToHeadFixtures[0]?.teams.home.logo} alt="home team logo" />
+        </Link>
         <H2HWinPctCircleBarHome headToHeadFixtures={headToHeadFixtures}/>
 
         <H2HDrawPctCircleBar headToHeadFixtures={headToHeadFixtures}/>
 
         <H2HWinPctCircleBarAway headToHeadFixtures={headToHeadFixtures}/>
-        <img className="object-contain w-12 h-12" src={headToHeadFixtures[0].teams.away.logo} alt="" />
+        <Link className="hover:opacity-70" to={`/team/${headToHeadFixtures[0].league.id}/${headToHeadFixtures[0].teams.away.id}/${getURLFriendlyString(headToHeadFixtures[0].teams.home.name)}`}>
+          <img className="object-contain w-12 h-12" src={headToHeadFixtures[0].teams.away.logo} alt="" />
+        </Link>
       </div>
     </div>
   )
